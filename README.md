f2abae1fad1779a59208b15b0447313edf55720c
uggletoggle <42126135+uggletoggle@users.noreply.github.com>
2020-07-22 23:05:23 -0300

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+# Socket-Server
+
+Reconstruir modulos node
+```
+npm install
+```
+
+Generar el dist/
+```
+
+tsc -w
+```
+
+Levantar servidor
+```
+nodemon dist/  ||  node dist/
+```
+
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from './clases/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true}));
+server.app.use( bodyParser.json() );
+
+// CORS
+server.app.use( cors({ origin: true, credentials: true }) );
+
+// Rutas
+server.app.use('/', router)
+
+server.start( () =>{
+
+    console.log('Server corriendo en el puerto ' + server.port);
+
+});
